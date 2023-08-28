a9cd11e569c0e66045a0250e79d85087322edbfb
Atilla Yasar <112716905+AtillaYasar@users.noreply.github.com>
2023-08-28 18:13:52 +0200

    Update snippets_for_later_use.py

--- snippets_for_later_use.py
+++ snippets_for_later_use.py
@@ -1,3 +1,7 @@
+"""
+for stuff ive written/implemented locally but not yet implemented in this repo
+"""
+
         def evalin_flag(flagged):
             program = flagged
             namespace = {}
@@ -6,3 +10,10 @@
 
             title = self.master['text']
             self.replace_flagged('evalout', str(result))
+
+        # in MyText.on_keypress in tkinter_text.py
+        elif keysym.startswith('F'):  # do something on f keys
+            flag = f'on {keysym.lower()}'
+            flagged = tkinter_functionality.get_flagged(self.get(1.0, 'end')[0:-1], flag)
+            if flagged != None:
+                self.run_script_lines(flagged)
